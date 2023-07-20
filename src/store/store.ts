45fcd8bd3cc792f5a8458238452296fa42affb13
EnTheos <theosenigma@gmail.com>
2023-07-20 16:32:47 +0300

    add fonts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../components/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    
   },
 });
 
